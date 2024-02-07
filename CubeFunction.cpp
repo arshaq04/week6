6917606d0a4c338a5de3676a9573aafe68ba7fff
Arshaq <arshahm2004@gmail.com>
2024-02-07 10:54:19 +0000

    Cube function

--- CubeFunction.cpp
+++ CubeFunction.cpp
@@ -4,7 +4,13 @@
 using namespace std;
 
 int main() {
-    
+    cout << "Enter a number and the result will be cubed." << endl;
+    int x;
+    cin >> x;
+
+    while (x != 0) {
+        
+    }
 
     return 0;
 }
